de3aaf5ee5d3668f66ab162282a0d808b9083b2a
Xiaotian Lu <rricolu@gmail.com>
2020-04-09 14:26:26 +1000

    Updates

---
+++ precache-manifest.586c6efc2dcc71d515ec8a2c614546d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722a37ba6584a83260022be8d2e26ed4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ecba30f07dad6bbc50df",
+    "url": "/monsters-rolodex/static/css/main.5b654729.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecba30f07dad6bbc50df",
+    "url": "/monsters-rolodex/static/js/main.9c961cba.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
